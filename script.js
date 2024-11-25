37073b5cdfb85fd6f6ad8b899bdbaa8d12f493b0
Ayush Tiwari <imayushtiwari3@gmail.com>
2024-11-25 20:50:52 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -3,37 +3,85 @@ const data = [
     name: "Disease detection advance",
     desc: "This project aims to make use of Machine Learning techniques to detect instances of Parkinson's Disease. The project performs the following tasks: \n1️⃣ Data Collection 2️⃣ Data Preprocessing\n3️⃣ Exploratory Data Analysis\n4️⃣ Dataset Balancing & Scaling\n5️⃣ Machine Learning Models Training & Evaluation",
     link: "https://github.com/iayushtiwari/disease-detection-advance",
-    image: "https://media.licdn.com/dms/image/sync/v2/D5627AQG3TbiFYcCemA/articleshare-shrink_160/articleshare-shrink_160/0/1730531590802?e=1733054400&v=beta&t=-U0FWrPnXt03ciyAP2MJnG2d3LbDZadp8iakMLKEU-k",
+    image:
+      "https://media.licdn.com/dms/image/sync/v2/D5627AQG3TbiFYcCemA/articleshare-shrink_160/articleshare-shrink_160/0/1730531590802?e=1733054400&v=beta&t=-U0FWrPnXt03ciyAP2MJnG2d3LbDZadp8iakMLKEU-k",
   },
   {
     name: "Traffic control",
     desc: "This project is an application of the Lambda Architecture that tracks the traffic safety and congestion of each street in city. It shows recent traffic crashes, red light and speed camera violations, and traffic patterns of 1,250 street segments within city limits. It was completed as the final project Sikhim Sikkim professional university - Big Data Application Architecture.",
     link: "https://github.com/iayushtiwari/Traffic-Control",
-    image: "https://media.licdn.com/dms/image/sync/v2/D5627AQFlLwI_a-FiBg/articleshare-shrink_160/articleshare-shrink_160/0/1730520572543?e=1733054400&v=beta&t=nrLJY3uhzMM7OokOac5ZuuDGPmS_EU8j8Sfdyf8TJTM",
+    image:
+      "https://media.licdn.com/dms/image/sync/v2/D5627AQFlLwI_a-FiBg/articleshare-shrink_160/articleshare-shrink_160/0/1730520572543?e=1733054400&v=beta&t=nrLJY3uhzMM7OokOac5ZuuDGPmS_EU8j8Sfdyf8TJTM",
   },
   {
     name: "Fake New Detection",
     desc: "The project aims to develop a machine-learning model capable of identifying andclassifying any news article as fake or not. The distribution of fake news can potentially have highly adverse effects on people and culture. This project involves building and training a model to classify news as fake news or not using a diverse dataset of news articles. We have used four techniques to determine the results of themodel",
     link: "https://githun.com/iayushtiwari/fakenewsdetection",
-    image: "https://media.licdn.com/dms/image/sync/v2/D5627AQFLjNHtvc3YfA/articleshare-shrink_160/articleshare-shrink_160/0/1730531814288?e=1733054400&v=beta&t=nOzf1R7AVo72BDmlnNEIELMi1qYaPCm7YhtbWMB_dIY",
+    image:
+      "https://media.licdn.com/dms/image/sync/v2/D5627AQFLjNHtvc3YfA/articleshare-shrink_160/articleshare-shrink_160/0/1730531814288?e=1733054400&v=beta&t=nOzf1R7AVo72BDmlnNEIELMi1qYaPCm7YhtbWMB_dIY",
   },
   {
     name: "Flipkart Review Sentiment Analysis",
     desc: "Flipkart is one of the most popular Indian companies. It is an e-commerce platform that competes with popular e-commerce platforms like Amazon. One of the most popularuse cases of data science is the task of sentiment analysis of product reviews sold on e-commerce platforms",
     link: "https://githun.com/iayushtiwari/flipkart-sentiment-analysis",
-    image: "https://media.licdn.com/dms/image/sync/v2/D5627AQEYycIJx3RvQA/articleshare-shrink_160/articleshare-shrink_160/0/1730531355544?e=1733054400&v=beta&t=daVdvAOfG1qgtvXRlj_z2keU6BoVVBemJOVhSK0vSYI",
+    image:
+      "https://media.licdn.com/dms/image/sync/v2/D5627AQEYycIJx3RvQA/articleshare-shrink_160/articleshare-shrink_160/0/1730531355544?e=1733054400&v=beta&t=daVdvAOfG1qgtvXRlj_z2keU6BoVVBemJOVhSK0vSYI",
   },
   {
     name: "Heart Disease Detection System",
     desc: "Logistic regression is a type of regression analysis in statistics used for prediction of outcome of a categorical dependent variable from a set of predictor or independent variables. In logistic regression the dependent variable is always binary. Logistic regression is mainly used to for prediction and also calculating the probability of success.",
     link: "https://github.com/iayushtiwari/Heart-Disease-Prediction-using-Logistic-Regression",
-    image: "https://media.licdn.com/dms/image/sync/v2/D5627AQEC0uihALnemw/articleshare-shrink_160/articleshare-shrink_160/0/1730530962815?e=1733054400&v=beta&t=TfzPFqQD_W7_VvuGpeq_CiqlmLSNmYoPTL31V2Fh7WM",
+    image:
+      "https://media.licdn.com/dms/image/sync/v2/D5627AQEC0uihALnemw/articleshare-shrink_160/articleshare-shrink_160/0/1730530962815?e=1733054400&v=beta&t=TfzPFqQD_W7_VvuGpeq_CiqlmLSNmYoPTL31V2Fh7WM",
   },
   {
     name: "Movie Recommendation",
     desc: "This is a Python-based movie recommendation system that implements text-retrieval techniques and Graphical User Interface. One special thing about this system is that its reommendations were tailored around users' emotion of the moment. There are so many existing movie recommender systems available in the market, but only a small number of them were designed based on users' psychological needs. The main objective of this project is to fill this gap by making traditional recommender system more user-driven",
     link: "https://github.com/iayushtiwari/movieRecommendationAccordingtoEmotions",
-    image: "https://media.licdn.com/dms/image/sync/v2/D5627AQF2b3KWj-YtMw/articleshare-shrink_160/articleshare-shrink_160/0/1730531121740?e=1733054400&v=beta&t=gp7OLoUw8Y2vcQr2Lv-GW9wcUakyZag75uB991uL7IE",
+    image:
+      "https://media.licdn.com/dms/image/sync/v2/D5627AQF2b3KWj-YtMw/articleshare-shrink_160/articleshare-shrink_160/0/1730531121740?e=1733054400&v=beta&t=gp7OLoUw8Y2vcQr2Lv-GW9wcUakyZag75uB991uL7IE",
+  },
+  {
+    name: "Notes - Notes Manager",
+    desc: "MERN full stack app with proper authentication and authorization, Application is to manage Notes with CRUD operation to Manage Own notes which securely possible with JWT and bcrypt MERN full stack app with proper authentication and authorization, Application is to manage Notes with CRUD operation to Manage Own notes which securely possible with JWT and bcrypt Skills: MongoDB · Express.js · React.js · Node.js · Cascading Style Sheets (CSS) · Bootstrap (Framework) · HTML5 · creative thinking · JSON Web Token (JWT) · Git · HTML · bcrypt",
+    link: "https://github.com/iayushtiwari/iNotebook",
+    image:
+      "https://media.licdn.com/dms/image/sync/v2/D5627AQF6n8-Ypt3_QQ/articleshare-shrink_160/articleshare-shrink_160/0/1730520959716?e=1733151600&v=beta&t=OCY9hvO3g_GpE_zZbx-FOwn7UxYuYLd20bEWHAGSakI",
+  },
+  {
+    name: "Stock Price Prediction",
+    desc: "Stock (also known as equity) is a security that represents the ownership of a fraction of a corporation. This entitles the owner of the stock to a proportion of the corporation's assets and profits equal to how much stock they own. Units of stock are called 'Shares.' A stock is a general term used to describe the ownership certificates of any company. Stock prices change everyday by market forces. By this we mean that share prices change because of supply and demand. If more people want to buy a stock (demand) than sell it(supply), then the price moves up. Conversely, if more people wanted to sell a stock than buy it, there would be greater supply than demand, and the price would fall. Understanding supply and demand is easy. So, why do stock prices change? The best answer is that nobody really knows for sure. Some believe that it isn't possible to predict how stocks will change in price while others think that by drawing charts and looking at past price movements, youcan determine when to buy and sell. The only thing we do know as a certainty is that stocks are volatile and can change in price extremely rapidly",
+    link: "https://github.com/iayushtiwari/stockPricePrediction",
+    image:
+      "https://media.licdn.com/dms/image/sync/v2/D5627AQHN_byih-skGA/articleshare-shrink_160/articleshare-shrink_160/0/1730531478306?e=1733151600&v=beta&t=GrOljN-6GU8rUwiojj5E2nnSNjKVPs_H3xuvdwCKU9I",
+  },
+  {
+    name: "Store Sales Prediction:",
+    desc: "This App aim to provide insight by doing predictive analysis of previous data it also conclude the important facts like festival, inflations, economy, back day and season sales, current political",
+    link: "https://github.com/iayushtiwari/store-sales-prediction-using-python",
+    image:
+      "https://media.licdn.com/dms/image/sync/v2/D5627AQE1G-Bcs9j3OQ/articleshare-shrink_160/articleshare-shrink_160/0/1730531410442?e=1733151600&v=beta&t=Vd9_88UZO2sndx_b-9LdW4xeULZ82WfBmrP5jsLA9BM",
+  },
+  {
+    name: "TED Talk Recommender",
+    desc: "Created a Ted talk recommendation app which recommends the ted talk you may like according to it’s popularity and user interest and behavior and mutual actions Created a Ted talk recommendation app which recommends the ted talk you may like according to it’s popularity and user interest and behavior and mutual actions Skills: Data Analytics · Jupyter · Python (Programming Language) · csv",
+    link: "https://github.com/iayushtiwari/tedtalkrecommender",
+    image:
+      "https://media.licdn.com/dms/image/sync/v2/D5627AQGhTEOzHSQ2_g/articleshare-shrink_160/articleshare-shrink_160/0/1730531253245?e=1733151600&v=beta&t=LNQ2kN4wkltbHk-BJx5aOSUYowfEwT7OgXxaMjmDmng",
+  },
+  {
+    name: "Wine Quality Detection",
+    desc: "This project focuses on predicting the quality of wines based on their physicochemical properties. This project explores various deep learning models to find the most effective approach for this prediction task, considering the nuances between different types of wines This project focuses on predicting the quality of wines based on their physicochemical properties. This project explores various deep learning models to find the most effective approach for this prediction task, considering the nuances between different types of wines",
+    link: "https://github.com/iayushtiwari/wine-quality-detection",
+    image:
+      "https://media.licdn.com/dms/image/sync/v2/D5627AQEul7PG5szz6g/articleshare-shrink_160/articleshare-shrink_160/0/1730531537647?e=1733151600&v=beta&t=l5sFCrUum3B9wdqb8sZzKkJmwUhlj7-67ls3HxRrArA",
+  },
+  {
+    name: "Disease Detection",
+    desc: "This project aims to provide which disease by doing Prescriptive analysis by data such as symptoms and weather",
+    link: "https://github.com/iayushtiwari/disease-detection",
+    image:
+      "https://media.licdn.com/dms/image/sync/v2/D5627AQF6DKWeiQhR1Q/articleshare-shrink_160/articleshare-shrink_160/0/1730531640146?e=1733151600&v=beta&t=-qwOpVswEB9o3RYAasMX5DlJZQ0zoM117K_mMUPyrvU",
   },
 ];
 
@@ -62,7 +110,7 @@ const data = [
 // );
 
 const proj = document.querySelector(".Projects");
-data.forEach((project) => {
+data.forEach((project, index) => {
   const div = document.createElement("div");
 
   // Create elements and append them to the div
@@ -85,17 +133,35 @@ data.forEach((project) => {
   link.textContent = "Read More";
   button.appendChild(link);
   div.appendChild(button);
-
+  if (index > 5) {
+    div.classList = ["none"];
+  }
   // Append the div to the proj element
   proj.appendChild(div);
 });
 
 // Create the final button and append it to proj
 const finalButton = document.createElement("button");
-const finalLink = document.createElement("a");
+// const finalLink = document.createElement("a");
 finalButton.classList = ["buttonLast"];
-finalLink.href = "https://github.com/iayushtiwari";
-finalLink.textContent = "Load More";
+// finalLink.href = "https://github.com/iayushtiwari";
+finalButton.textContent = "Load More";
 // ... add the icon here if needed
-finalButton.appendChild(finalLink);
+// finalButton.appendChild(finalLink);
 proj.appendChild(finalButton);
+
+const loadMore = document.querySelector(".buttonLast");
+loadMore.addEventListener("click", function()   
+ {const projWithDisplayNoneArray = document.querySelectorAll(".none");
+    // Code to be executed when the button is clicked   
+    projWithDisplayNoneArray.forEach((div) => {
+      div.classList.remove("none");
+    });
+    loadMore.classList.add("none")
+    console.log("Ok xlixk e",projWithDisplayNoneArray)
+});
+
+document.querySelector(".menu").addEventListener("click",function(){
+const nav = document.querySelector("nav");
+nav.style.display = nav.style.display=="none"?"unset":"none";
+})
